3c1ddf0ca28c858c84ea7b93150d6122ba1c0858
Ricky Chaudhary <21bcs082@nith.ac.in>
2024-01-27 09:23:25 +0530

    Final Commit

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,14 +1,38 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { log } from 'console';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((term) =>{
+     console.log(`Searching... ${term}`);
+     
+
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    if(term){
+      params.set('query', term);
+    }
+    else{
+      params.delete('query');
+    }
+    replace (`${pathname}?${params.toString()}`);
+  },300);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
       <input
+        onChange={(e) => {handleSearch(e.target.value)}}
+        defaultValue={searchParams.get('query')?.toString()}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
       />
